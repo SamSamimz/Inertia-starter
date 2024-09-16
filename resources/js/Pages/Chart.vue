f5be2e909a4f65092617f7b8c6d1c68cacbac0c3
SamSamimz <samsamim595@gmail.com>
2024-09-16 23:30:36 +0600

    vue chartjs

---
+++ resources/js/Pages/Chart.vue
@@ -0,0 +1,34 @@
+<template>
+  <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
+</template>
+
+  <script setup>
+import { Bar } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+import { reactive } from "vue";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
+
+const props = defineProps({
+  chartData: Object,
+});
+
+const chartOptions = reactive({
+  responsive: true,
+});
+</script>
--- routes/web.php
+++ routes/web.php
@@ -9,26 +9,45 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::inertia('/','Home')->name('home')->middleware('auth');
 
+Route::middleware('auth')->group(function () {
+    Route::inertia('/','Home')->name('home');
 
-Route::get('/users', function(Request $request) {
-    return inertia('Users', [
+    Route::get('/users', function(Request $request) {
+        return inertia('Users', [
         'users' => User::when($request->search, function($query) use ($request) {
             $query->where('name', 'LIKE', '%'.$request->search.'%')
-            ->orWhere('email', 'LIKE' , '%'.$request->search.'%');
-        })->paginate(5)->withQueryString(),
-        'searchTerm' => $request->search
-    ]);
-})->name('users')->middleware('auth');
+                ->orWhere('email', 'LIKE' , '%'.$request->search.'%');
+            })->paginate(5)->withQueryString(),
+            'searchTerm' => $request->search
+        ]);
+    })->name('users');
 
-Route::inertia('/about','About')->name('about');
-Route::post('/logout',LogoutController::class)->name('logout')->middleware('auth');
-Route::delete('/delete/{id}', function($id) {
-    $user = User::findOrFail($id);
-    $user->delete();
-    return to_route('home')->with('success','User deleted!');
-})->name('delete');
+    Route::inertia('/about','About')->name('about');
+    Route::post('/logout',LogoutController::class)->name('logout');
+    Route::delete('/delete/{id}', function($id) {
+        $user = User::findOrFail($id);
+        $user->delete();
+        return to_route('home')->with('success','User deleted!');
+    })->name('delete');
+    Route::inertia('/chart','Chart',[
+        'chartData' => [
+            'labels' => [
+                "January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"
+            ],
+            'datasets' => [
+                [
+                    'label' => 'Example Dataset',
+                    'data' => [20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130],
+                    'backgroundColor' => 'rgba(75, 192, 192, 0.2)',
+                    'borderColor' => 'rgba(75, 192, 192, 1)',
+                    'borderWidth' => 1,
+                ],
+            ],
+        ],
+    ])->name('chart');
+});
 Route::middleware('guest')->group(function () {
     Route::inertia('/login','Auth/Login')->name('login');
     Route::post('/login',LoginController::class);
